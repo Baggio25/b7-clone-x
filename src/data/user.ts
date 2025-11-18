7096da911b16e9c4ae8ec12f0b3354e53bb2893d
Rodrigo Baggio <rodrigo.baggio.si@gmail.com>
2025-11-18 12:50:30 -0300

    feat: add fake user

---
+++ src/data/user.ts
@@ -0,0 +1,11 @@
+import { User } from "@/types/user";
+
+export const user: User = {
+  slug: 'baggio',
+  name: 'Rodrigo Baggio',
+  avatar: 'https://ui-avatars.com/api/?background=random&name=Baggio&size=100',
+  cover: '',
+  bio: 'Programador',
+  link: 'https://b7web.com.br',
+  postCount: 127
+}
