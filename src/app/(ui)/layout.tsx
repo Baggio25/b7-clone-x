ad869283c9734fd3347d99317be51d78832d543c
Baggio25 <rodrigo.baggio.si@gmail.com>
2025-11-18 21:43:22 -0300

    feat: add layout  left part 3

--- src/app/(ui)/layout.tsx
+++ src/app/(ui)/layout.tsx
@@ -3,6 +3,7 @@ import Logo from "../components/ui/logo";
 import NavItem from "../components/nav/nav-item";
 import { faHouse, faUser } from "@fortawesome/free-solid-svg-icons";
 import NavLogout from "../components/nav/nav-logout";
+import NavMyProfile from "../components/nav/nav-myprofile";
 
 type Props = {
   children: ReactNode;
@@ -12,26 +13,19 @@ const Layout = ({ children }: Props) => {
   return (
     <main className="min-h-screen flex justify-center mx-auto max-w-7xl">
       <section
-        className="flex lg:flex flex-col sticky
+        className="hidden lg:flex flex-col sticky
         top-0 h-screen w-72 px-3 border-r-2 border-gray-900"
       >
         <div className="flex-1 mt-6">
           <Logo size={24} />
           <nav className="mt-11">
-            <NavItem 
-              href="/home"
-              icon={faHouse}
-              label="Página Inicial"
-            />
-            <NavItem 
-              href="/profile"
-              icon={faUser}
-              label="Meu Perfil"
-            />
+            <NavItem href="/home" icon={faHouse} label="Página Inicial" />
+            <NavItem href="/profile" icon={faUser} label="Meu Perfil" />
           </nav>
         </div>
         <div className="mb-6 flex flex-col gap-4">
           <NavLogout />
+          <NavMyProfile />
         </div>
       </section>
       <section className="flex-1 max-w-lg">{children}</section>
