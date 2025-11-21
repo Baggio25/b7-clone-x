a1dc2e6145ff88e9361dfe61f430c322e0e1f5c8
Baggio25 <rodrigo.baggio.si@gmail.com>
2025-11-20 22:59:04 -0300

    feat: add post component

--- src/app/(ui)/home/page.tsx
+++ src/app/(ui)/home/page.tsx
@@ -1,11 +1,13 @@
 import HomeHeader from "@/app/components/home/home-header";
+import TweetPost from "@/app/components/tweet/tweet-post";
 
 const HomePage = () => {
-  return( 
+  return (
     <div>
       <HomeHeader />
+      <TweetPost />
     </div>
-  )
+  );
 };
 
 export default HomePage;
