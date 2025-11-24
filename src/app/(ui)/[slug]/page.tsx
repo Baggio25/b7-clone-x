25af067e3d7749828d771bd5d0547424517f2135
Baggio25 <rodrigo.baggio.si@gmail.com>
2025-11-23 22:27:47 -0300

    feat: add perfil page 2

--- src/app/(ui)/[slug]/page.tsx
+++ src/app/(ui)/[slug]/page.tsx
@@ -1,7 +1,13 @@
+import Button from "@/app/components/ui/button";
 import GeneralHeader from "@/app/components/ui/general-header";
 import { user } from "@/data/user";
+import { faLink } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
 
 const PerfilPage = () => {
+  const isMe = true;
+
   return (
     <div>
       <GeneralHeader backHref="/">
@@ -12,7 +18,48 @@ const PerfilPage = () => {
         <div
           className="bg-gray-500 h-28 bg-no-repeat bg-cover bg-center"
           style={{ backgroundImage: "url(" + user.cover + ")" }}
-        ></div>
+        />
+        <div className="-mt-12 flex justify-between items-end px-6">
+          <img
+            src={user.avatar}
+            alt={user.name}
+            className="size-24 rounded-full"
+          />
+          <div className="w-32">
+            {isMe && (
+              <Link href={`/${user.slug}/edit`}>
+                <Button label="Editar perfil" size={2} />
+              </Link>
+            )}
+
+            {!isMe && <Button label="Seguir" size={2} />}
+          </div>
+        </div>
+        <div className="px-6 mt-4">
+          <div className="text-xl font-bold">{user.name}</div>
+          <div className="text-gray-500">@{user.slug}</div>
+          <div className="py-5 text-lg text-gray-500">{user.bio}</div>
+          {user.link && (
+            <div className="flex gap-2 items-center">
+              <FontAwesomeIcon icon={faLink} className="size-5" />
+              <Link
+                href={user.link}
+                target="_blank"
+                className="text-blue-300 text-xs"
+              >
+                {user.link}
+              </Link>
+            </div>
+          )}
+          <div className="my-5 flex gap-6 ">
+            <div className="text-xl text-gray-500">
+              <span className="text-white">99</span> Seguindo
+            </div>
+            <div className="text-xl text-gray-500">
+              <span className="text-white">99</span> Seguidores
+            </div>
+          </div>
+        </div>
       </section>
     </div>
   );
